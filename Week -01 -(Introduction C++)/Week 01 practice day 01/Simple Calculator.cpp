11f334ffd791e1983c79b9fd61880c7499897fed
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-23 16:08:43 +0600

    pitron practice

---
+++ Week -01 -(Introduction C++)/Week 01 practice day 01/Simple Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,sum;
+    cin >> x>>y;
+    sum=x+y;
+    cout << sum<<endl;
+int     multi=x*y;
+        cout << multi<<endl;
+ int   sub =x-y;
+    cout << sub<<endl;
+
+}
