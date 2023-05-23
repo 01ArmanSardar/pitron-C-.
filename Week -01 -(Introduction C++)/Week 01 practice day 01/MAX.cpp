2a90b3b80f4f8777397806bc5689bc320d4ea662
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-23 23:26:29 +0600

    pitron practice

--- Week -01 -(Introduction C++)/Week 01 practice day 01/MAX.cpp
+++ Week -01 -(Introduction C++)/Week 01 practice day 01/MAX.cpp
@@ -1,1 +1,14 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,mx;
+    cin >>n;
+    int ar[n];
+    for (int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+       mx= max(ar[i]);
+cout<<mx;
+    }
+}
