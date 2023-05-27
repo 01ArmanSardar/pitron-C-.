e333a90dc4cdedb763b00031c8fecd46012a5d05
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-27 13:50:20 +0600

    pitron practice

--- Week -01 -(Introduction C++)/mid term exam/Question 2 -- C.cpp
+++ Week -01 -(Introduction C++)/mid term exam/Question 2 -- C.cpp
@@ -1,5 +1,24 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+class Person
+{
+    public :
+    char name[150];
+    float height ;
+    int age ;
+    //constructor
+    Person(int a,float h,char*n)
+    {
+    age=a;
+    height=h;
+    strcpy (name,n);
+    }
+};
 int main()
 {
-    int
+    char name [150]="barkat Ullah";
+    Person* barkat=new Person(45,4.5,name);
+    cout << barkat ->age<<endl;
+    cout << barkat->height<<endl;
+    cout<<barkat->name<<endl;
 }
