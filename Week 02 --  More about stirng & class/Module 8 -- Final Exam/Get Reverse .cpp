9aef7fcc927217a2ce9381f0d8468079cc9244e9
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-05 19:57:12 +0600

    pitron practice

--- Week 02 --  More about stirng & class/Module 8 -- Final Exam/Get Reverse .cpp
+++ Week 02 --  More about stirng & class/Module 8 -- Final Exam/Get Reverse .cpp
@@ -1,1 +1,20 @@
-
+#include <bit/stdc++.h>
+using namespace std;
+class Student
+{
+    public
+    string name ;
+    int roll ;
+    int math_marks ;
+    int eng_marks;
+};
+bool compare (Student x,Studenty)
+{
+    if (x.marks<y.marks) return true ;
+    else return false ;
+}
+int main()
+{
+   Student [3];
+   for (int i)
+}
