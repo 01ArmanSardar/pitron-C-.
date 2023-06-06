0e6da5b204efd6df33839b441132848ee7f340d5
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-06 22:36:09 +0600

    pitron practice

--- Week 02 --  More about stirng & class/Module 8 -- Final Exam/Replalce It.cpp
+++ Week 02 --  More about stirng & class/Module 8 -- Final Exam/Replalce It.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    string s,x;
+    cin >>s>>x;
+    while (s.find(x)!=-1)
+    {
+        s.replace (s.find (x),s.length(),"$");
 
+    }
+    cout <<s<<endl;
+}
+//2
+//thatsagoodmomentforgoodrelations good
+//canyoutellmewhereicanfindheriwillbegreatefultoyouifyoutellmetheanswer you
