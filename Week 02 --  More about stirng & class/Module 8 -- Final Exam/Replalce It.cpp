31d25b3bf34bcdcc8c6fc0fd73364b6fccc4cd70
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-07 20:47:20 +0600

    pitron practice

--- Week 02 --  More about stirng & class/Module 8 -- Final Exam/Replalce It.cpp
+++ Week 02 --  More about stirng & class/Module 8 -- Final Exam/Replalce It.cpp
@@ -2,15 +2,24 @@
 using namespace std ;
 int main()
 {
+    string good,you;
     string s,x;
     cin >>s>>x;
-    while (s.find(x)!=-1)
+    while (s.find(good)!=-1)
     {
-        s.replace (s.find (x),s.length(),"$");
+        s.replace (s.find (good),s.length(),"$");
 
     }
     cout <<s<<endl;
+
+    while (x.find(you)!=-1)
+    {
+        s.replace (x.find (you),x.length(),"$");
+
+    }
+    cout <<x<<endl;
 }
+
 //2
 //thatsagoodmomentforgoodrelations good
 //canyoutellmewhereicanfindheriwillbegreatefultoyouifyoutellmetheanswer you
