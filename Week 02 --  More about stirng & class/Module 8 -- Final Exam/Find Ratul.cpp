f928f0e67c430491cb700e351e55219ae3bdc4b5
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-07 21:01:36 +0600

    pitron practice

--- Week 02 --  More about stirng & class/Module 8 -- Final Exam/Find Ratul.cpp
+++ Week 02 --  More about stirng & class/Module 8 -- Final Exam/Find Ratul.cpp
@@ -1,1 +1,23 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline (cin,s);
+    stringstream ss(s);
+    string word;
+int     flag =0;
+    while (ss>>word)
+    {
+        if  (word == "Ratul")
+            flag=1;
+    }
+    if (flag =1)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+}
